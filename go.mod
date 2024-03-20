f950eb421b622307bf3b2489722b33dd2e7b30d7
rizkyharahap <rizkyhh1999@gmail.com>
2024-03-21 02:53:37 +0700

    upgrade module version and fix breaking changes

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/rizkyharahap/try-go-module-app
 
 go 1.22.1
 
-require github.com/rizkyharahap/try-go-module v1.1.0
+require (
+	github.com/rizkyharahap/try-go-module/v2 v2.0.0
+)
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	tryGoModule "github.com/rizkyharahap/try-go-module"
+	tryGoModule "github.com/rizkyharahap/try-go-module/v2"
 )
 
 func main() {
-	fmt.Println(tryGoModule.SayHello("Rizki"))
+	fmt.Println(tryGoModule.SayHello())
 }
