92b93b57c05a23c8a05856031f57bc9010e61948
rizkyharahap <rizkyhh1999@gmail.com>
2024-03-21 02:37:54 +0700

    add module and use say hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/rizkyharahap/try-go-module-app
+
+go 1.22.1
+
+require github.com/rizkyharahap/try-go-module v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	tryGoModule "github.com/rizkyharahap/try-go-module"
+)
+
+func main() {
+	fmt.Println(tryGoModule.SayHello("Rizki"))
+}
